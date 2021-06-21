02d51b11e20a516068b6c736affa4d79ecdaad34
AronBA <72823328+AronBA@users.noreply.github.com>
2021-06-21 14:48:00 +0200

    èbung 1 mit tkinter

--- W10/Übung 1.py
+++ W10/Übung 1.py
@@ -2,12 +2,19 @@ import os
 from tkinter import *
 dir = "../testdir"
 
-def search(dir, text):
+def search(dir):
+    text = e.get()
     for file in os.listdir(dir):
         if file.endswith(".txt"):
             f = open(f"../testdir/{file}", "r")
             if text in f.read():
-                print(f"Ich habe das gesucht in {file} gefunden")
+                print(f"Ich hab es gefunden in {file}")
+                l = Label(root, text=file)
+                l.pack()
+
+
+
+
 
 
 
@@ -15,8 +22,11 @@ def search(dir, text):
 text = "hello"
 
 root = Tk()
-e = Entry(root)
-s = Button(root, text="Search", command= lambda : search(dir, text))
+e = Entry(root,bd =5)
+s = Button(root, text="Search", command= lambda : search(dir))
+l1 = Label(root, text="Es wurde in folgenden Dateien gefunden:")
 e.pack()
 s.pack()
+l1.pack()
+
 root.mainloop()
